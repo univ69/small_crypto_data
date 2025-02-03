628c15e72e7a9687967a251f2946fd6ab9c4e7cc
univ69 <zinyshen24@gmail.com>
2025-02-03 03:27:19 +0200

    мелкие доработки

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     result = database_func.get_btc_price()
-    btc_price, timestamp = result[0], result[-1]
+    change_result = database_func.get_btc_price_change()
+    btc_price = result
     # Возвращаем HTML-шаблон с результатом
-    return render_template('welcome.html', btc_price=btc_price, timestamp=timestamp)
+    return render_template('welcome.html', btc_price=btc_price, change_result=change_result)
+
