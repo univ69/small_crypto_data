4e41241b539fc2f59b03c9fde0435a89a59db697
univ69 <zinyshen24@gmail.com>
2025-02-03 02:03:00 +0200

    обработчик главной страницы

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import render_template, Flask
-
+from modules import database_func
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return render_template('welcome.html')
-
-
-#app.run()
+    result = database_func.get_btc_price()
+    btc_price, timestamp = result[0], result[-1]
+    # Возвращаем HTML-шаблон с результатом
+    return render_template('welcome.html', btc_price=btc_price, timestamp=timestamp)
